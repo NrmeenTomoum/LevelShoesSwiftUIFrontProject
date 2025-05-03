6d13a4eb5e6d53531dd8c09f5b79d070d26d27df
Nermeen Tomoum <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2025-05-03 13:56:17 +0400

    Initial Commit

---
+++ ListOfProducts/ListOfProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfProductsApp.swift
+//  ListOfProducts
+//
+//  Created by Nermeen Tomoum on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ListOfProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListOfProductsTests/ListOfProductsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfProductsTests.swift
+//  ListOfProductsTests
+//
+//  Created by Nermeen Tomoum on 03/05/2025.
+//
+
+import Testing
+@testable import ListOfProducts
+
+struct ListOfProductsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
